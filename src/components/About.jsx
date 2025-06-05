413d81f09bba6c12dd8470006ea1a74b421b47d0
sanath-2512 <sanathwaraikar@gmail.com>
2025-06-05 19:49:57 +0530

    minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,14 +23,14 @@ const About = () => {
         
         <h1>🎬 Features</h1>
         <ul>
-        <li>Search movies using The Movie <strong> TMDB API</strong></li>
+        <li>Search movies using The Movie name</li>
         <li>See detailed movie info like release date, popularity, rating, and budget</li>
         <li>Watch trailers via <strong>YouTube</strong></li>
         <li>Add or remove movies from your watchlist (with <strong>localStorage support</strong>)</li>
         </ul>
         <h1>👨‍💻 Developer</h1>
         <p>
-        This project was built by <strong>Sanath Waraikar</strong>, a CS & AI student passionate about frontend and full-stack development. Viewly is part of his <strong>React learning journey.</strong>
+        This project was built by <strong>Sanath P Waraikar</strong>, a CS & AI student passionate about frontend and full-stack development. Viewly is part of his <strong>React learning journey.</strong>
         </p>
         <h1>📬 Get in Touch</h1>
         <p>Have feedback or questions? Feel free to connect:</p>
